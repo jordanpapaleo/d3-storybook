3a71a736f4383c85defeb47e30938a99685f7cd8
Jordan Papaleo <papaleowebdev@gmail.com>
2019-05-01 11:42:12 -0700

    added storybook, fix scripts,  and ran them

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,9 @@
-import { configure } from '@storybook/html';
+import { configure } from '@storybook/html'
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context('../stories', true, /\.stories\.js$/)
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach((filename) => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,11 +5,15 @@ import { storiesOf } from '@storybook/html'
 
 storiesOf('Demo', module)
   .add('hi ho', () => require('./test.html'))
-  // .add('heading', () => '<h1>Hello World</h1>')
-  // .add('button', () => {
-  //   const button = document.createElement('button')
-  //   button.type = 'button'
-  //   button.innerText = 'Hello Button'
-  //   button.addEventListener('click', e => console.log(e))
-  //   return button
-  // });
+  .add('hi ho', () => require('./test.html'))
+  .add('hi ho', () => require('./test.html'))
+  .add('hi ho', () => require('./test.html'))
+  .add('hi ho', () => require('./test.html'))
+// .add('heading', () => '<h1>Hello World</h1>')
+// .add('button', () => {
+//   const button = document.createElement('button')
+//   button.type = 'button'
+//   button.innerText = 'Hello Button'
+//   button.addEventListener('click', e => console.log(e))
+//   return button
+// });
